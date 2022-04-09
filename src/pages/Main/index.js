0c6b98999d1bb500886f0cc460c9159f5a75f667
Маратик555 <amr404@bk.ru>
2022-04-09 16:03:06 +0300

    ok

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,40 +1,42 @@
-import {useState, useEffect, useRef} from 'react';
-import socket from '../../socket';
-import ACTIONS from '../../socket/actions';
-import {useHistory} from 'react-router';
-import {v4} from 'uuid';
+import {useState, useEffect, useRef} from 'react'
+import socket from '../../socket'
+import ACTIONS from '../../socket/actions'
+import {useHistory} from 'react-router'
+import {v4} from 'uuid'
 
 export default function Main() {
-  const history = useHistory();
-  const [rooms, updateRooms] = useState([]);
-  const rootNode = useRef();
+  const history = useHistory()
+  const [rooms, updateRooms] = useState([])
+  const rootNode = useRef()
 
   useEffect(() => {
     socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
       if (rootNode.current) {
-        updateRooms(rooms);
+        updateRooms(rooms)
       }
-    });
-  }, []);
+    })
+  }, [])
 
   return (
-    <div ref={rootNode}>
+    <div className="join-block" ref={rootNode}>
       <h1>Available Rooms</h1>
-
+      <br/>
       <ul>
         {rooms.map(roomID => (
           <li key={roomID}>
             {roomID}
-            <button onClick={() => {
-              history.push(`/room/${roomID}`);
-            }}>JOIN ROOM</button>
+            <button className="btn btn-outline-info" onClick={() => {
+              history.push(`/room/${roomID}`)
+            }}>JOIN ROOM
+            </button>
           </li>
         ))}
       </ul>
 
-      <button onClick={() => {
-        history.push(`/room/${v4()}`);
-      }}>Create New Room</button>
+      <button className="btn btn-outline-dark" onClick={() => {
+        history.push(`/room/${v4()}`)
+      }}>Create New Room
+      </button>
     </div>
-  );
+  )
 }
